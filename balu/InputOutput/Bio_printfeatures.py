fb8152a963ffe68bcf63ed5c0b1d5a90b3b5857e
Diego Patiño <dipaco@gmail.com>
2016-08-02 18:17:06 -0500

    Bio_plotfeature added

---
+++ balu/InputOutput/Bio_printfeatures.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+
+def Bio_printfeatures(X, *args):
+    """
+     Bio_printfeatures(X, Xn)    # for features with feature names
+     Bio_printfeatures(X)        # for feature values only
+
+     Toolbox: Balu
+        Display extracted features.
+        Xn: feature names  (matrix mxp, one row per each string of p
+            characters)
+        X:  feature values (vector 1xm for m features)
+        Xu: feature units  (matrix mxq, one row per each string of q
+            characters)
+
+        These variables are the outputs of Bgeofeatures or Bintfeatures.
+
+        The output of Bio_printfeatures is like this:
+
+        1 center of grav i       [pixels]         163.297106
+        2 center of grav j       [pixels]         179.841850
+        3 Height                 [pixels]         194.000000
+        4 Width                  [pixels]         196.000000
+        5 Area                   [pixels]         29361.375000
+        :         :           :                  :
+
+       Example 1: Display of standard geometric features of testimg1.jpg
+          I = imread('testimg1.jpg');            # input image
+          R = Bsegbalu(I);                       # segmentation
+          [X,Xn,Xu] = Bfg_standard(R);           # standard geometric features
+          Bprintfeatures(X,Xn,Xu)
+
+       Example 2: Display of first 5 samples of datagauss.mat
+          load datagauss
+          Xn = ['[length]';'[weigh] '];
+          Xu = ['cm';'kg'];
+          for i=1:5
+             fprintf('Sample %d:\n',i);
+             Bprintfeatures(X(i,:),Xn,Xu)
+             Benterpause
+          end
+
+
+       See also Bplotfeatures.
+
+     (c) D.Mery, PUC-DCC, 2010
+     http://dmery.ing.puc.cl
+
+     With collaboration of:
+     Diego Patiño (dapatinoco@unal.edu.co) -> Translated implementation into python
+    """
+
+    N = np.max(X.shape)
+    if len(args) > 0:
+        Xn = args[0]
+    else:
+        Xn = N * ['']
+
+    for k in range(len(Xn)):
+        print '{0} {1} {2}'.format(k, Xn[k], np.squeeze(X)[k])
--- balu_test.py
+++ balu_test.py
@@ -1,5 +1,6 @@
 import numpy as np
 from balu.FeatureExtraction import Bfx_fourier
+from balu.InputOutput import Bio_printfeatures
 from scipy.ndimage import imread
 from matplotlib.pyplot import imshow, show
 
@@ -7,4 +8,5 @@ I = (imread('test_images/Lenna.png')[:, :, 1]).astype(float) / 256.0
 R = np.ones(I.shape)
 options = {'Mfourier': 64, 'Nfourier': 64, 'mfourier': 2, 'nfourier': 2}
 
-print Bfx_fourier(I, options)
+X, Xn = Bfx_fourier(I, options)
+Bio_printfeatures(X, Xn)
