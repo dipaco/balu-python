41c558fa454761c806a5b76346bff8ac5f715baf
Diego Patino <dipaco@gmail.com>
2016-08-18 02:23:31 -0500

    Added command FeatureExtraction/Bfx_all

---
+++ balu/FeatureExtraction/Bfx_all.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+
+def Bfx_all(I, R=None, options=None):
+    '''% [X,Xn] = Bfx_all(I,R,options)
+    [X,Xn] = Bfx_all(I,options)
+
+    Toolbox: Balu
+       All pixels.
+
+       X is the features vector, Xn is the list feature names (see Example to
+       see how it works).
+
+
+       Example:
+          I = imread('testimg1.jpg')             # input image
+          X, Xn = Bfx_all(I)                   # all pixels
+
+
+    (c) D.Mery, PUC-DCC, 2012
+    http://dmery.ing.puc.cl
+
+    With collaboration from:
+    Diego Patiño (dapatinoco@unal.edu.co) -> Translated implementation into python (2016)
+    '''
+
+    X = np.zeros((1, I.size))
+    X[0, :] = I.ravel()
+    Xn = X.size * ['']
+    return X, Xn
--- balu/FeatureExtraction/__init__.py
+++ balu/FeatureExtraction/__init__.py
@@ -1,5 +1,6 @@
 from Bfx_area import Bfx_area
 from Bfx_geo import Bfx_geo
 from Bfx_fourier import Bfx_fourier
+from Bfx_all import Bfx_all
 
-__all__ = ['Bfx_area', 'Bfx_geo', 'Bfx_fourier']
+__all__ = ['Bfx_area', 'Bfx_geo', 'Bfx_fourier', 'Bfx_all']
